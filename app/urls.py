053ad7d26c3630f8602046ffa69d85dfd046e396
delta <amin138400138400@gmail.com>
2024-12-23 17:34:40 +0330

    Edit in views.py

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.urls import path
 from django.conf.urls.static import static
-from .views import GetProductsView, AddProductView, GetCategoriesView, GetProductByIdView,AddProfile
+from .views import GetProductsView, AddProductView, GetCategoriesView, GetProductByIdView, AddProfile
 
 urlpatterns = [
                   path('getallProduct/', GetProductsView.as_view()),
@@ -9,4 +9,4 @@ urlpatterns = [
                   path('GetCategories/', GetCategoriesView.as_view()),
                   path('registerUser/', AddProfile.as_view()),
                   path('AddProduct/', AddProductView.as_view()),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
