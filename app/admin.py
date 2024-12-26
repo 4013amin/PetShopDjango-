a10a0797b7a0e47df52b6b546f027525e0cdb9b8
delta <amin138400138400@gmail.com>
2024-12-26 20:33:33 +0330

    Edit in views.py

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category,Users
+from .models import Product, Category, Users
 
 
 # Register your models here.
@@ -11,6 +11,7 @@ class PostAdmin(admin.ModelAdmin):
 @admin.register(Users)
 class profileUsers(admin.ModelAdmin):
     list_display = ('phone', 'password')
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
