89f30cac01acc4598ddb34ffdd89028a6e2b6d1a
delta <amin138400138400@gmail.com>
2025-04-23 13:31:14 +0330

    Edit and push in server liara

--- ShopApp/settings.py
+++ ShopApp/settings.py
@@ -95,7 +95,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [REDIS_URL],
+            "hosts": [("redis-1234.liara.run", 6379)],
         },
     },
 }
